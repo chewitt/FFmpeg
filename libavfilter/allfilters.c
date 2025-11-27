tao-harald/rpi-ffmpeg

Add V4L2 M2M deinterlace & scale filters
These have a lot a code in common so this is a single file

libavfilter/allfilters.c
@@ -256,6 +256,7 @@ extern const AVFilter ff_vf_derain;
 extern const AVFilter ff_vf_deshake;
 extern const AVFilter ff_vf_deshake_opencl;
 extern const AVFilter ff_vf_despill;
+extern const AVFilter ff_vf_deinterlace_v4l2m2m;
 extern const AVFilter ff_vf_detelecine;
 extern const AVFilter ff_vf_dilation;
 extern const AVFilter ff_vf_dilation_opencl;
@@ -433,6 +434,7 @@ extern const AVFilter ff_vf_scale;
 extern const AVFilter ff_vf_scale_cuda;
 extern const AVFilter ff_vf_scale_npp;
 extern const AVFilter ff_vf_scale_qsv;
+extern const AVFilter ff_vf_scale_v4l2m2m;
 extern const AVFilter ff_vf_scale_vaapi;
 extern const AVFilter ff_vf_scale_vt;
 extern const AVFilter ff_vf_scale_vulkan;

chewitt/FFmpeg

libavfilter/allfilters.c
@@ -252,6 +252,7 @@ extern const FFFilter ff_vf_dedot;
 extern const FFFilter ff_vf_deflate;
 extern const FFFilter ff_vf_deflicker;
 extern const FFFilter ff_vf_deinterlace_qsv;
+extern const FFFilter ff_vf_deinterlace_v4l2m2m;
 extern const FFFilter ff_vf_deinterlace_vaapi;
 extern const FFFilter ff_vf_dejudder;
 extern const FFFilter ff_vf_delogo;
@@ -442,6 +443,7 @@ extern const FFFilter ff_vf_scale_cuda;
 extern const FFFilter ff_vf_scale_d3d11;
 extern const FFFilter ff_vf_scale_npp;
 extern const FFFilter ff_vf_scale_qsv;
+extern const FFFilter ff_vf_scale_v4l2m2m;
 extern const FFFilter ff_vf_scale_vaapi;
 extern const FFFilter ff_vf_scale_vt;
 extern const FFFilter ff_vf_scale_vulkan;